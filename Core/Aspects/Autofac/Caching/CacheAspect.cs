ed64febc890965dd688a34fde00834a85c5bfb7c
Türker Karapetti <trkrkrl53@gmail.com>
2022-08-11 23:07:56 +0300

    commit-8-caching altyapısı hazırlandı

---
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -0,0 +1,54 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Caching;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheAspect : MethodInterception
+    {
+        private int _duration;
+        private ICacheManager _cacheManager;
+
+        public CacheAspect(int duration = 60)
+        {
+            _duration = duration;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var methodName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");
+            var arguments = invocation.Arguments.ToList();
+            var key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";//cachekey burada olusturulur
+            if (_cacheManager.IsAdd(key))
+            {
+                invocation.ReturnValue = _cacheManager.Get(key);//cachede var database e gitme demek
+                return;
+            }
+            invocation.Proceed();
+            _cacheManager.Add(key, invocation.ReturnValue, _duration);//cachemanagere key i buradan gönderir
+        }
+        /*Cache aspecte süre vermezsen default 60min
+         * 
+        Cachemanager olarak service tool kullanarak hangi cache manageri kullandığını berlirt:ICache manager—
+        (ki bu interface di burada hangisine bağlarsan Microsoft veya redis onu kkullanır)
+        Bu yüzden bu sayfada kod değiştirmeyiz  eğer redise geçersek sadece coremodule de o iş
+         * 
+         * git bak bakalım bellekte böyle bir cache anahtarrı key varmı
+        Varsa  =sen methodu hiç çalıştırmadan şimdi geri dön-cachede var ordan al diyor
+        Else
+        Git invocationu devam ettir methodu devam ettir
+        Veritabanına gidecek
+        Cache eklenecek
+        Anahtar,return value ve duration eklenmiş olacak
+         * 
+         */
+    }
+}
---
+++ Core/CrossCuttingConcerns/Caching/ICacheManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Caching
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key);
+        object Get(string key);
+
+        void Add(string key, object value, int duration);
+        bool IsAdd(string key);//cache de varmı kontrolü
+        void Remove(string key);
+        void RemoveByPattern(string pattern);// burrada patternden kastı- fonksiyon adında get varmı veya category varmı,
+
+        //method adinda boyle şeyler olanları remove et-uçur yani
+
+    }
+}
